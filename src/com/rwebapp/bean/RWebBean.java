59e6b479b4a794400070ebc38ef3576343aa24a1
Jia <realyoungdj@gmail.com>
2017-11-26 22:50:58 +0100

    modified index.xhtml and RWebBean.java

--- src/com/rwebapp/bean/RWebBean.java
+++ src/com/rwebapp/bean/RWebBean.java
@@ -1,5 +1,8 @@
 package com.rwebapp.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name="RWebBean")
@@ -7,15 +10,24 @@ import javax.faces.bean.ManagedBean;
 public class RWebBean {
 	
 	private double carPrice;
+	private List<Double> priceList;
+	private String inputPriceString;
 
 	public RWebBean() {
+		this.initData();
 		this.createRConnection();
+	}
+	
+	private void initData() {
+		priceList = new ArrayList<>();
+		inputPriceString = "User Input: ";
 	}
 	
 	private void createRConnection() {
 		
 	}
 	
+	// getter and setter
 	public double getCarPrice() {
 		return this.carPrice;
 	}
@@ -24,7 +36,28 @@ public class RWebBean {
 		this.carPrice = carPrice;
 	}
 	
+	public List<Double> getPriceList() {
+		return this.priceList;
+	}
+	
+	public void setPriceList(List<Double> priceList) {
+		this.priceList = priceList;
+	}
+	
+	public String getInputPriceString() {
+		return inputPriceString;
+	}
+	
+	public void setInputPriceString(String inputPriceString) {
+		this.inputPriceString = inputPriceString;
+	}
+	
+	// Bean Method
 	public String addCarPrice() {
+		// check User Input
+		
+		priceList.add(new Double(this.getCarPrice()));
+		this.inputPriceString += this.getCarPrice() + " ";
 		return null;
 	}
 	
@@ -33,6 +66,8 @@ public class RWebBean {
 	}
 	
 	public String deleteAllData() {
+		priceList.clear();
+		this.inputPriceString = "User Input: ";
 		return null;
 	}
 }
