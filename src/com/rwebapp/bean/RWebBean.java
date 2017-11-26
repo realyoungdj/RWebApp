3efcb4ee458a63380af22f8f0ad74f5a91e0a36c
Jia <realyoungdj@gmail.com>
2017-11-26 21:14:34 +0100

    create Web app java bean and JSF configuration

---
+++ src/com/rwebapp/bean/RWebBean.java
@@ -0,0 +1,16 @@
+package com.rwebapp.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="RWebBean")
+
+public class RWebBean {
+
+	public RWebBean() {
+		this.createRConnection();
+	}
+	
+	private void createRConnection() {
+		
+	}
+}
