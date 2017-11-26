25e3760855bc15f92c801147f343222efec30552
Jia <realyoungdj@gmail.com>
2017-11-26 20:48:05 +0100

    TestR is a java client to connect Rserve, and show the result in Console

--- src/com/rwebapp/test/TestR.java
+++ src/com/rwebapp/test/TestR.java
@@ -1,5 +1,25 @@
 package com.rwebapp.test;
 
-public class TestR {
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
 
+public class TestR {
+	public static void main(String[] args) throws RserveException, REXPMismatchException {
+		// Before run this program, 
+		// Start Rserve in RStudio.
+		// Rserve(args)
+		// to shutdown, in command line 
+		// killall Rserve
+		RConnection c = new RConnection();
+		REXP x = c.eval("seq(-5, 5, by=.5)");
+		if (x.isVector())
+		{
+			System.out.println("vector");
+			double tmp[] = x.asDoubles();
+			for (int i = 0; i < tmp.length; i++)
+			System.out.println(tmp[i]);
+		}
+	}
 }
